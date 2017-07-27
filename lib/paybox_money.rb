83e9538a4c843d1afc9aa623a086c55d4975d3be
Pavel Tkachenko <tpepost@gmail.com>
2017-07-27 19:11:51 +0600

    Add Payment::Status

--- lib/paybox_money.rb
+++ lib/paybox_money.rb
@@ -3,6 +3,7 @@ require 'paybox_money/constants'
 require 'paybox_money/utility'
 require 'paybox_money/signature'
 require 'paybox_money/payment/init'
+require 'paybox_money/payment/status'
 
 module PayboxMoney
   # Your code goes here...
--- lib/paybox_money/constants.rb
+++ lib/paybox_money/constants.rb
@@ -1,1 +1,2 @@
 GATEWAY_INIT_PAYMENT = 'https://www.paybox.kz/init_payment.php'.freeze
+GATEWAY_STATUS_PAYMENT = 'https://www.paybox.kz/get_status.php'.freeze
